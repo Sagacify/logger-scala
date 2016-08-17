80dab3d8a95bb045f7c90346a6bb70b12edec82d
Augustin Borsu <a.borsu@gmail.com>
2016-08-17 20:36:36 +0200

    fix(LoggerSpec): Hide test logs from stdout

--- src/test/scala/logger/LoggerSpec.scala
+++ src/test/scala/logger/LoggerSpec.scala
@@ -12,14 +12,29 @@ import Converter.map2JValue
 
 case class Poney(name: String, age: Int)
 
-class LoggerSpec extends FlatSpec  {
+class LoggerSpec extends FlatSpec {
+
+  def hideSTDOut(testCode: (ByteArrayOutputStream) => Unit) {
+    // this bypasses the stdout in the tests and makes it available
+    val oldOut = System.out
+    val out = new ByteArrayOutputStream()
+    val psOut = new PrintStream(out)
+
+    try {
+      Console.withOut(psOut) { testCode(out) }
+    }
+    finally {
+      out.close
+      psOut.close
+    }
+  }
 
   def lazyString(): String = {
     throw new java.lang.Exception("This should not have been evaluated.")
     "Yipee!"
   }
 
-  "A logger" should "lazy evaluate what it logs" in {
+  "A logger" should "lazy evaluate what it logs" in hideSTDOut { (out) =>
     val l = new Logger("test")
     l.trace(lazyString)
 
@@ -28,43 +43,28 @@ class LoggerSpec extends FlatSpec  {
     }
   }
 
-  it should "be able to serialize errors" in {
+  it should "be able to serialize errors" in hideSTDOut { (out) =>
     val l = new Logger("test")
     val e = new java.lang.Exception("Excetpion test")
     l.warn("test", e)
   }
 
-  it should "be able to serialize a Map " in {
+  it should "be able to serialize a Map " in hideSTDOut { (out) =>
     val l = new Logger("test")
     l.warn("test", Map("test" -> 5)) // age mental hein!
   }
 
-  it should "be able to serialize case classes" in {
+  it should "be able to serialize case classes" in hideSTDOut { (out) =>
     val l = new Logger("test")
     l.warn("test", extract(Poney(name="Louis", age=5))) // age mental hein!
   }
 
-  it should "serialize date following ISO-8601 format" in {
+  it should "serialize date following ISO-8601 format" in hideSTDOut { (out) =>
     val log = new Logger("test")
-    val oldOut = System.out
-    try {
-      val out = new ByteArrayOutputStream()
-      val psOut = new PrintStream(out)
-      Console.setOut(psOut)
-
-      log.warn("test")
-      val printed = out.toString
-      oldOut.println(printed)
-
-      val pattern = Pattern.compile("\"time\":\"20\\d\\d-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d\\d\\dZ\"")
-      val matcher = pattern.matcher(printed)
-
-      assert(matcher.find())
-
-      out.close
-      psOut.close
-    } finally {
-      Console.setOut(oldOut)
-    }
+    log.warn("test")
+    val printed = out.toString
+    val pattern = Pattern.compile("\"time\":\"20\\d\\d-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d\\d\\dZ\"")
+    val matcher = pattern.matcher(printed)
+    assert(matcher.find())
   }
 }
